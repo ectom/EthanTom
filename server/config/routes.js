c11ebac1d0ef5f4f1da7ef4f1cb26b69a6fabd37
Ethan <etom579@gmail.com>
2018-01-06 19:21:21 -0800

    contact email working with ses/nodemailer

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,14 +1,8 @@
 var path = require('path')
-var polls = require('../controllers/polls.js')
 var sendMail = require('../controllers/sendMail.js');
 
 module.exports = function(app){
 	app.post('/send', sendMail.send); // send mail with contact form
-	app.get('/polls', polls.getAll);   // get all polls
-	app.post('/polls', polls.createPoll);  // create poll
-	app.get('/polls/delete/:id', polls.destroy);   //get one poll
-	app.get('/polls/:id', polls.getOne);   //get one poll
-	app.get('/vote/:id/:str', polls.vote); //vote
 	app.all("*", (req, res, next) => {
 		res.sendFile(path.resolve("./public/dist/index.html"))
 	});
--- server/controllers/sendMail.js
+++ server/controllers/sendMail.js
@@ -1,25 +1,30 @@
 var mongoose = require('mongoose');
 var nodemailer = require('nodemailer');
+var ses = require('nodemailer-ses-transport');
 var Contact = mongoose.model('Contact');
 require('dotenv').config();
 
 module.exports = {
     send: function(req, res) {
-        const transporter = nodemailer.createTransport({
-            port: 465,
-            secure: true, // true for 465, false for other ports
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_ADDR,
-                pass: process.env.EMAIL_PASS
-            }
-        });
+
+        const transporter = nodemailer.createTransport(ses({
+            accessKeyId: process.env.ACCESS_KEY_ID,
+            secretAccessKey: process.env.SECRET_ACCESS_KEY
+        }));
 
         const mailOptions = {
-            from: process.env.EMAIL_ADDR,
-            to: process.env.EMAIL_ADDR,
+            from: "no-reply@ethanchristophertom.com",
+            to: 'ethanchristophertom@gmail.com',
             subject: req.body.subject,
-            text: 'Name: ' + req.body.firstName + ' ' + req.body.lastName + '\nEmail: ' + req.body.emailAddress + '\nMessage:\n' + req.body.message
+            text: 'Name: ' + req.body.firstName + ' ' + req.body.lastName + '\nEmail: ' + req.body.emailAddress + '\nMessage:\n' + req.body.message,
+            // cc:,
+            // bcc:,
+            // attachments: [{
+            //     filename: 'My Cool Document',
+            //     path: 'https://path/to/cool-document.docx',
+            //     contentType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+            // }]
+
         };
 
         transporter.sendMail(mailOptions, function(error, info) {
@@ -31,34 +36,34 @@ module.exports = {
         });
         // ---------------- save to database ------------------------
         const contact = new Contact(req.body);
-        const find = Contact.findOne({emailAddress: req.body.email});
-        if (find == undefined) {
-            contact.save()
-                .then(() => {
-                    console.log('successfully added contact');
-                    res.json(true);
-                })
-                .catch(errors => res.status(450).json(errors));
-        } else {
-            Contact.findOneAndUpdate(
-                req.body.email
-            , {
-                $addToSet: {
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    subject: req.body.subject,
-                    message: req.body.message
-                }
-            }).exec(function(err, db_res) {
-                if (err) {
-                    throw err;
-                    console.log('error', err)
-                    res.status(500).json(err)
-                } else {
-                    console.log(db_res);
-                    res.json(true)
-                }
+        // const find = Contact.findOne({emailAddress: req.body.email});
+        // if (find == undefined) {
+        contact.save()
+            .then(() => {
+                console.log('successfully added contact');
+                res.json(true);
             })
-        }
+            .catch(errors => res.status(450).json(errors));
+        // } else {
+        //     Contact.findOneAndUpdate(
+        //         req.body.emailAddress,
+        //         {
+        //         $addToSet: {
+        //             firstName: req.body.firstName,
+        //             lastName: req.body.lastName,
+        //             subject: req.body.subject,
+        //             message: req.body.message
+        //         }
+        //     }).exec(function(err, db_res) {
+        //         if (err) {
+        //             throw err;
+        //             console.log('error', err)
+        //             res.status(500).json(err)
+        //         } else {
+        //             console.log(db_res);
+        //             res.json(true)
+        //         }
+        //     })
+        // }
     }
 }
